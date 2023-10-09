9139368d7f6bc3818e9c24510a6c67ba6c197d4f
Wendel Fabian Chinsamy <wendel@Wendels-MBP.lan>
2023-10-09 12:01:37 +0200

    fix test

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -19,6 +19,6 @@ describe('MainContent', () => {
 
     const button = screen.getByRole('button');
     await userEvent.click(button);
-    exect(screen.getByTestId('help-area')).toBeInTheDocument();
+    expect(screen.getByTestId('help-area')).toBeInTheDocument();
   });
 });
