b808d111a5f3d0570ce6cd8c8f9e0d12602d7318
Wendel Fabian Chinsamy <wendel@Wendels-MBP.lan>
2023-10-09 13:09:37 +0200

    break test

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -19,6 +19,6 @@ describe('MainContent', () => {
 
     const button = screen.getByRole('button');
     await userEvent.click(button);
-    expect(screen.getByTestId('help-area')).toBeInTheDocument();
+    exect(screen.getByTestId('help-area')).toBeInTheDocument();
   });
 });
